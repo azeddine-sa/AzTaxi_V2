afaf555d70f5c4314d410dddcefa7186bf4097ec
SAOUTI Azeddine <saouti.azeddine@gmail.com>
2021-11-22 13:48:22 +0100

    menu

---
+++ app/src/main/java/be/azsa/aztaxi_v3/MainActivity.java
@@ -0,0 +1,24 @@
+package be.azsa.aztaxi_v3;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //optional
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
