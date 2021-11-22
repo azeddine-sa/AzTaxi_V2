8b7a34a8424101b4ae38d9e8bf1abf9484de3038
SAOUTI Azeddine <saouti.azeddine@gmail.com>
2021-11-22 14:54:04 +0100

    init fragment

--- app/src/main/java/be/azsa/aztaxi_v3/MainActivity.java
+++ app/src/main/java/be/azsa/aztaxi_v3/MainActivity.java
@@ -1,24 +1,82 @@
 package be.azsa.aztaxi_v3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //optional
+        //Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Navigation View
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        //Menu
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new BookFragment()).commit();
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_book:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new BookFragment()).commit();
+                break;
+            case R.id.nav_history:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HistoryFragment()).commit();
+                break;
+            case R.id.nav_profil:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfilFragment()).commit();
+                break;
+            case R.id.nav_contact_us:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContactUsFragment()).commit();
+                break;
+
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
