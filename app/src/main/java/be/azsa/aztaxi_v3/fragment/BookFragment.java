e81804ca18a2fec8f0d6740089210c6809bce824
SAOUTI Azeddine <saouti.azeddine@gmail.com>
2021-12-05 16:45:20 +0100

    Date & Time Picker Booking Fragment

--- app/src/main/java/be/azsa/aztaxi_v3/fragment/BookFragment.java
+++ app/src/main/java/be/azsa/aztaxi_v3/fragment/BookFragment.java
@@ -1,6 +1,10 @@
 package be.azsa.aztaxi_v3.fragment;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,7 +12,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -27,8 +34,12 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.SimpleTimeZone;
 
 import be.azsa.aztaxi_v3.MainActivity;
 import be.azsa.aztaxi_v3.R;
@@ -36,11 +47,17 @@ import be.azsa.aztaxi_v3.R;
 public class BookFragment extends Fragment {
 
     private Long idUser;
-    private EditText book_depart;
-    private EditText book_destination;
-    private LocalDate book_when;
-    private EditText book_infos;
+    private EditText book_depart, book_destination, book_infos ;
+    private TextView book_date, book_time;
     private Button book_commander;
+
+    //Date&Time Picker
+    Calendar calendar = Calendar.getInstance();
+    final int year = calendar.get(Calendar.YEAR);
+    final int month = calendar.get(Calendar.MONTH);
+    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+    int tHour;
+    int tMinute;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Nullable
@@ -48,16 +65,71 @@ public class BookFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_book, container, false);
 
+        //initialistion
         book_depart = (EditText) view.findViewById(R.id.et_book_depart);
         book_destination = (EditText) view.findViewById(R.id.et_book_destination);
-        book_when = LocalDate.now();
         book_infos = (EditText) view.findViewById(R.id.et_book_infos);
-
+        book_date = (TextView) view.findViewById(R.id.tv_book_date);
+        book_time = (TextView) view.findViewById(R.id.tv_book_time);
         book_commander = (Button) view.findViewById(R.id.btn_book_commander);
+
+        //onClickListener
+        book_date.setOnClickListener(book_date_listener);
+        book_time.setOnClickListener(book_time_listener);
         book_commander.setOnClickListener(book_commander_listener);
 
         return view;
     }
+
+    //DatePicker
+    private View.OnClickListener book_date_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int day) {
+                    month = month+1;
+                    String date = year+"-"+month+"-"+day;
+
+                    //mise en forme de la date (pattern)
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    try {
+                        Date date1 = sdf.parse(date);
+                        book_date.setText(sdf.format(date1));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                };
+            },year,month,day);
+            datePickerDialog.show();
+        }
+    };
+
+    private View.OnClickListener book_time_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), android.R.style.Theme_Holo_Dialog_MinWidth, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    tHour = hourOfDay;
+                    tMinute = minute;
+                    String time = tHour+":"+tMinute;
+
+                    //mise en forme de l'heure (pattern)
+                    SimpleDateFormat f24hours = new SimpleDateFormat("HH:mm");
+                    try {
+                        Date date2 = f24hours.parse(time);
+                        book_time.setText(f24hours.format(date2));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, 24, 00,true);
+            timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            timePickerDialog.updateTime(tHour,tMinute);
+            timePickerDialog.show();
+        }
+    };
 
     private View.OnClickListener book_commander_listener  = new View.OnClickListener() {
         @Override
